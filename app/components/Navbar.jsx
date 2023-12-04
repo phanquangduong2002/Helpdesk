c53fe2d5c73b0d9c4701849eb34f285581fcecd5
phanquangduong2002 <phanquangduong2002@gmail.com>
2023-12-05 02:23:54 +0700

    commit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,11 +1,20 @@
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
+import Logo from '../../public/logo.svg'
 
 export default function Navbar() {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt="Helpdesk Logo"
+        width={72}
+        height={72}
+        quality={100}
+      />
       <h1>Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
     </nav>
-  );
+  )
 }
